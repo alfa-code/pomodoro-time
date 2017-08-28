89c840cb07c92f5958a3c6a2aac494071e0e9630
Hydrock <hydrock74@gmail.com>
2017-08-29 00:49:25 +0300

    add css modules

--- src/app/components/header/index.jsx
+++ src/app/components/header/index.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import style from './style.scss'
+
 export default class Header extends Component {
   render() {
     return (
-      <header className="header-block">
+      <header className={style.header}>
         That is Header
       </header>
     );
--- src/app/components/main-container/index.jsx
+++ src/app/components/main-container/index.jsx
@@ -5,7 +5,7 @@ import Header from '@src/app/components/header';
 export default class MainContainer extends Component {
   render() {
     return (
-      <div className="">
+      <div>
         <Header/>
       </div>
     );
