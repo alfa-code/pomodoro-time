4a0985d806213967a4d691926b3e3b752cd346e6
Hydrock <hydrock74@gmail.com>
2017-08-29 00:43:00 +0300

    minor fixes

---
+++ src/app/components/main-container/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Header from '@src/app/components/header';
+
+export default class MainContainer extends Component {
+  render() {
+    return (
+      <div className="">
+        <Header/>
+      </div>
+    );
+  }
+}
