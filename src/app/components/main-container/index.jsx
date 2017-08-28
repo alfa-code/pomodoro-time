7bdf6a73dd118491a8f8d5fb7ddba74b1a2dd15f
Hydrock <hydrock74@gmail.com>
2017-08-29 01:31:12 +0300

    add fonts and logo component

--- src/app/components/main-container/index.jsx
+++ src/app/components/main-container/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '@src/app/components/header';
+import Logo from '@src/app/components/logo';
 
 import style from './style.scss'
 
@@ -7,6 +8,7 @@ export default class MainContainer extends Component {
   render() {
     return (
       <div className={style.container}>
+        <Logo/>
         <Header/>
       </div>
     );
