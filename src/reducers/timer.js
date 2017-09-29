12a82aecd4b72faac0774709a0b2c721be3d7621
Hydrock <hydrock74@gmail.com>
2017-09-29 20:37:53 +0300

    add timer period and break perid cookies

--- src/reducers/timer.js
+++ src/reducers/timer.js
@@ -1,12 +1,25 @@
 import * as constants from '@src/constants.js'
+import * as Cookies from "js-cookie";
+
+let period = Cookies.get('timerPeriod');
+if (!period) {
+  Cookies.set('timerPeriod', 25);
+  period = 25;
+}
+
+let breakTime = Cookies.get('timerBreak');
+if (!breakTime) {
+  Cookies.set('timerBreak', 5);
+  breakTime = 5;
+}
 
 let startState = {
   timerTime: 0,
   timerState: 'pause', // working, pause // It determines whether the countdown is in progress or not
   mode: constants.TIMER_MODE_POMODORO, //pomodoro, break // timer mode
   timerActivated: false, // Started timer or not
-  period: 0.2, // minutes
-  breakTime: 0.1, // minutes
+  period: period, // minutes
+  breakTime: breakTime, // minutes
   timeStart: Date.now(),
   timeEnd: 0,
   timeDifference: 0
