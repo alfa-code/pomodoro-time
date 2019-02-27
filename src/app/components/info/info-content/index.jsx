67e844665e625e8883691d6bb5868eb3f3b4d9db
Alex Vechkanov <hydrock74@gmail.com>
2019-02-27 22:08:42 +0300

    add new text

--- src/app/components/info/info-content/index.jsx
+++ src/app/components/info/info-content/index.jsx
@@ -1,41 +1,46 @@
-import React from 'react';
+import React from "react";
 
 // styles
-import style from './style.scss';
+import style from "./style.scss";
 
-const email = 'hydrochk@yandex.ru';
+const email = "hydrock@yandex.ru";
 const text = [
-  'Hello! My name is Alex or Hydrock (Github).',
-  'If you have any comments or suggestions, you can send them to the address',
-  'You can also send proposals directly to the project on the ',
+  "Hey there! I’m Alex, a full-time web-developer who is passionate about creating efficient user-friendly websites and apps.",
+  "If you have any comments or suggestions just drop me an email to (",
+  ") or contact me on Github (",
+  ")."
 ];
 
 export default function InfoContent() {
   return (
     <div className={style.container}>
-      <div className={style.row}>
-        <img
-          src="https://avatars2.githubusercontent.com/u/20814779?s=460&v=4"
-          alt="avatar"
-          className={style.avatar}
-        />
-        <div className={style.row}>
-          {text[0]}
+        <div className={style.info}>
+            <img
+                src="https://avatars2.githubusercontent.com/u/20814779?s=460&v=4"
+                alt="avatar"
+                className={style.avatar}
+            />
+            <div>
+                <div className={style.row}>
+                    {text[0]}
+                </div>
+                <div className={style.row}>
+                    {text[1]}
+                    <a href={`mailto:${email}`}>
+                        hydrochk@yandex.ru
+                    </a>
+                    {text[2]}
+                    <a
+                        href="https://github.com/Hydrock/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        https://github.com/Hydrock/
+                    </a>
+                    {text[3]}
+                </div>
+            </div>
         </div>
-        <div className={style.row}>
-          {text[1]} <a href={`mailto:${email}`}>hydrochk@yandex.ru</a>
-        </div>
-        <div className={style.row}>
-          {text[2]}
-          <a
-            href="https://github.com/Hydrock/pomodoro"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            github
-          </a>
-        </div>
-      </div>
     </div>
   );
 }
