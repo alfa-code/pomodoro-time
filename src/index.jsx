ceec297e029364f0b35843b536470628ab19e2da
Alex Vechkanov <hydrock74@gmail.com>
2020-03-17 16:52:46 +0300

    added the react route and secret path

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
 
 // components
 import MainContainer from 'src/app/components/main-container';
@@ -14,9 +19,20 @@ import 'src/styles';
 
 const App = function App() {
     return (
-        <Provider store={store}>
-            <MainContainer />
-        </Provider>
+        <Router>
+            <Provider store={store}>
+                <Switch>
+                    <Route path="/secret">
+                        <div>
+                            <iframe src="https://testjmb.alfabank.ru/ona/auth/login" title="https" width="1200" height="800"/>
+                        </div>
+                    </Route>
+                    <Route path="/">
+                        <MainContainer />
+                    </Route>
+                </Switch>
+            </Provider>
+        </Router>
     );
 };
 
